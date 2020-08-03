a172875df82b84c9b70f824e822d1aee2f77614c
SylvainSenechal <senechalsylvain@hotmail.com>
2020-08-04 00:07:09 +0200

    refactored without class

--- javascriptVersion/index.js
+++ javascriptVersion/index.js
@@ -8,30 +8,22 @@ ctx.font = '15px serif'
 const DRAWING_OFFSET_X = 50
 const DRAWING_OFFSET_Y = 50
 
-const SAMPLE_PER_PIXEL = 4
+const SAMPLE_PER_PIXEL = 1
 const IMAGE_WIDTH = 400
 const ASPECT_RATIO = 16.0 / 9.0
 const IMAGE_HEIGHT = Math.floor(IMAGE_WIDTH / ASPECT_RATIO)
 
 
-class Vec3 {
-  constructor(x, y, z) {
-    this.x = x
-    this.y = y
-    this.z = z
-  }
+// Vector [x, y, z]
 
-  add = (vec) => new Vec3(this.x + vec.x, this.y + vec.y, this.z + vec.z)
-  sub = (vec) => new Vec3(this.x - vec.x, this.y - vec.y, this.z - vec.z)
-  mul = (val) => new Vec3(this.x * val, this.y * val, this.z * val)
-  div = (val) => new Vec3(this.x / val, this.y / val, this.z / val)
-
-  dot = (vec) => this.x * vec.x + this.y * vec.y + this.z * vec.z
-  lengthSquared = () => this.x * this.x + this.y * this.y + this.z * this.z
-  length = () => Math.sqrt(this.lengthSquared())
-  unitVector = () => this.div(this.length())
-}
-
+const add = (vec1, vec2) => vec1.map((val, index) => val + vec2[index])
+const sub = (vec1, vec2) => vec1.map((val, index) => val - vec2[index])
+const mul = (vec1, value) => vec1.map((val, index) => val * value)
+const div = (vec1, value) => vec1.map((val, index) => val / value)
+const dot = (vec1, vec2) => vec1.reduce((acc, val, index) => acc + val * vec2[index], 0)
+const lengthSquared = vec => vec.reduce((acc, val) => acc + val * val, 0)
+const length = vec => Math.sqrt(lengthSquared(vec))
+const unitVector = vec => div(vec, length(vec))
 
 class Camera {
   constructor() {
@@ -40,119 +32,77 @@ class Camera {
     this.viewportWidth = this.aspectRatio * this.viewportHeight
     this.focalLength = 1.0
 
-    this.origin = new Vec3(0, 0, 0)
-    this.horizontal = new Vec3(this.viewportWidth, 0, 0)
-    this.vertical = new Vec3(0.0, this.viewportHeight, 0.0)
-    this.lowerLeftCorner = this.origin.sub(this.horizontal.div(2)).sub(this.vertical.div(2)).sub(new Vec3(0, 0, this.focalLength))
+    this.origin = [0, 0, 0] // new Vec3(0, 0, 0)
+    this.horizontal = [this.viewportWidth, 0, 0] // new Vec3(this.viewportWidth, 0, 0)
+    this.vertical = [0, this.viewportHeight, 0] // new Vec3(0.0, this.viewportHeight, 0.0)
+    // this.lowerLeftCorner = this.origin.sub(this.horizontal.div(2)).sub(this.vertical.div(2)).sub(new Vec3(0, 0, this.focalLength))
+    this.lowerLeftCorner = sub(sub(sub(this.origin, div(this.horizontal, 2)), div(this.vertical, 2)), [0, 0, this.focalLength])
+    console.log(this.lowerLeftCorner)
+    let a = sub(this.origin, div(this.horizontal, 2))
+    console.log(div(this.horizontal, 2))
   }
-
-  getRay = (u, v) => new Ray(this.origin, this.lowerLeftCorner.add(this.horizontal.mul(u)).add(this.vertical.mul(v)).sub(this.origin))
+  getRay = (u, v) => ({origin: this.origin, direction: sub(add(add(this.lowerLeftCorner, mul(this.horizontal, u)), mul(this.vertical, v)), this.origin)})
 }
 
-class Color {
-  constructor(r, g, b) {
-    this.r = r
-    this.g = g
-    this.b = b
-  }
 
-  draw = (ctx, x, y) => {
-    this.divThis(SAMPLE_PER_PIXEL)
-    ctx.fillStyle = `rgb(${this.clamp(this.r, 0.0, 0.999) * 256}, ${this.clamp(this.g, 0.0, 0.999) * 256}, ${this.clamp(this.b, 0.0, 0.999) * 256})`
-    ctx.fillRect(x, y, 1, 1)
-  }
 
-  // clamp = (x, min, max) => x < min ? min : x > max ? : max : x
-  clamp = (x, min, max) => {
-    if (x < min) return min
-    if (x > max) return max
-    return x
-  }
-
-  add = (col) => new Color(this.r + col.r, this.g + col.g, this.b + col.b)
-  mul = (val) => new Color(this.r * val, this.g * val, this.b * val)
-  div = (val) => new Color(this.r / val, this.g / val, this.b / val)
-
-  addThis = (col) => {
-    this.r += col.r
-    this.g += col.g
-    this.b += col.b
-  }
-  // mul = (val) => new Color(this.r * val, this.g * val, this.b * val)
-  mulThis = (val) => {
-    this.r *= val
-    this.g *= val
-    this.b *= val
-  }
-  divThis = (val) => {
-    this.r /= val
-    this.g /= val
-    this.b /= val
-  }
-
+const clamp = (x, min, max) => {
+  if (x < min) return min
+  if (x > max) return max
+  return x
 }
 
-class Ray {
-  constructor(origin, direction) {
-    this.origin = origin
-    this.direction = direction
-  }
+const drawPixel = (pixelColor, ctx, x, y) => {
+  div(pixelColor, SAMPLE_PER_PIXEL)
+  ctx.fillStyle = `rgb(${clamp(pixelColor[0], 0.0, 0.999) * 256}, ${clamp(pixelColor[1], 0.0, 0.999) * 256}, ${clamp(pixelColor[2], 0.0, 0.999) * 256})`
+  ctx.fillRect(x, y, 1, 1)
+}
 
-  at = t => this.origin.add(this.direction.mul(t))
-
-  rayColor = world => {
-    let rec = new HitRecord()
-    // if (world.hit(this, 0, Infinity, rec)) {
-    let [hit, returnedRecord] = world.hit(this, 0, Infinity, rec)
-    if (hit) {
-      let color = new Color(returnedRecord.normal.x, returnedRecord.normal.y, returnedRecord.normal.z)
-      return color.add(new Color(1, 1, 1)).mul(0.5)
-    }
-    let unitDirection = this.direction.unitVector()
-    let t = 0.5 * unitDirection.y + 1.0
-    return new Color(1, 1, 1).mul(1.0 - t).add(new Color(0.5, 0.7, 1.0).mul(t))
-  }
-
-  // rayColor = () => {
-  //   let t = hitSphere(new Vec3(0, 0, -1), 0.5, this)
-  //   if (t > 0.0) {
-  //     let N = this.at(t).sub(new Vec3(0, 0, -1)).unitVector()
-  //     return new Color(N.x + 1, N.y + 1, N.z + 1).mul(0.5)
-  //   }
-  //   let unitDirection = this.direction.unitVector()
-  //   t = 0.5 * unitDirection.y + 1.0
-  //   return new Color(1, 1, 1).mul(1.0 - t).add(new Color(0.5, 0.7, 1.0).mul(t))
+const rayColor = (r, world) => {
+  // let t = hitSphere([0,0,-1], 0.5, r)
+  // if (t > 0) {
+  //   let n = unitVector(sub(at(r, t), [0,0,-1]))
+  //   return mul([n[0] + 1, n[1] + 1, n[2] + 1,], 0.5)
   // }
+  // let unitDirection = unitVector(r.direction)
+  // t = 0.5 * unitDirection[1] + 1.0
+  // return add(mul([1, 1, 1], (1.0 - t)), mul([0.5, 0.7, 1.0], t))
+
+
+  let rec = new HitRecord()
+  let [hit, returnedRecord] = world.hit(r, 0, Infinity, rec)
+  if (hit) {
+    let color = [returnedRecord.normal[0] + 1, returnedRecord.normal[1] + 1, returnedRecord.normal[2] + 1]
+    return mul(color, 0.5)
+    // return add(color, mul([1, 1, 1], 0.5))
+  }
+  let unitDirection = unitVector(r.direction) // this.direction.unitVector()
+  let t = 0.5 * unitDirection[1] + 1.0
+  return add(mul([1, 1, 1], (1.0 - t)), mul([0.5, 0.7, 1.0], t))
 }
+
+const at = (r, t) => add(r.origin, mul(r.direction, (t)))
 
 const hitSphere = (center, radius, r) => {
-  let oc = r.origin.sub(center)
-  let a = r.direction.lengthSquared()
-  let halfB = oc.dot(r.direction)
-  let c = oc.lengthSquared() - radius * radius
+  let oc = sub(r.origin, center)
+  let a = lengthSquared(r.direction)
+  let halfB = dot(oc, r.direction)
+  let c = lengthSquared(oc) - radius * radius
   let discriminant = halfB * halfB - a * c
   return discriminant < 0 ? - 1.0 : (- halfB - Math.sqrt(discriminant)) / a
 }
 
 class HitRecord {
   constructor() {
-    this.p = new Vec3(0, 0, 0)
-    this.normal = new Vec3(0, 0, 0)
+    this.p = [0, 0, 0] // new Vec3(0, 0, 0)
+    this.normal = [0, 0, 0] // new Vec3(0, 0, 0)
     this.t = 0
     this.frontFace = false
   }
 
   setFaceNormal = (r, outwardNormal) => {
-    // if (r.direction.dot(outwardNormal) < 0) {
-    //   this.frontFace = true
-    // } else {
-    //   this.frontFace = false
-    // }
-    // if (this.frontFace === true) {
-    //   this.normal =
-    // }
-    this.frontFace = r.direction.dot(outwardNormal) < 0
-    this.normal = this.frontFace ? outwardNormal : outwardNormal.mul(-1)
+    this.frontFace = dot(r.direction, outwardNormal) < 0
+    this.normal = this.frontFace ? outwardNormal : mul(outwardNormal, - 1)
   }
 }
 
@@ -163,10 +113,10 @@ class Sphere {
   }
 
   hit = (r, tMin, tMax, rec) => {
-    let oc = r.origin.sub(this.center)
-    let a = r.direction.lengthSquared()
-    let halfB = oc.dot(r.direction)
-    let c = oc.lengthSquared() - this.radius * this.radius
+    let oc = sub(r.origin, this.center)
+    let a = lengthSquared(r.direction)
+    let halfB = dot(oc, r.direction)
+    let c = lengthSquared(oc) - this.radius * this.radius
     let discriminant = halfB * halfB - a * c
 
     if (discriminant > 0) {
@@ -174,16 +124,16 @@ class Sphere {
       let temp = (- halfB - root) / a
       if (temp < tMax && temp > tMin) {
         rec.t = temp
-        rec.p = r.at(rec.t)
-        let outwardNormal = (rec.p.sub(this.center).div(this.radius))
+        rec.p = at(r, rec.t)
+        let outwardNormal = div(sub(rec.p, this.center), this.radius) // (rec.p.sub(this.center).div(this.radius))
         rec.setFaceNormal(r, outwardNormal)
         return [true, rec]
       }
       temp = (-halfB + root) / a
       if (temp < tMax && temp > tMin) {
         rec.t = temp
-        rec.p = r.at(rec.t)
-        let outwardNormal = (rec.p.sub(this.center).div(this.radius))
+        rec.p = at(r, rec.t)
+        let outwardNormal = div(sub(rec.p, this.center), this.radius)
         rec.setFaceNormal(r, outwardNormal)
         return [true, rec]
       }
@@ -212,11 +162,6 @@ class HittableList {
         closestSoFar = returnedRecord.t
         rec = returnedRecord
       }
-      // if (object.hit(r, tMin, closestSoFar, tempRec)) {
-      //   hitAnything = true
-      //   closestSoFar = tempRec.t
-      //   rec = tempRec
-      // }
     })
 
     return [hitAnything, rec]
@@ -227,25 +172,22 @@ const render = (ctx, canvas) => {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   let camera = new Camera()
   let world = new HittableList()
-  world.add(new Sphere(new Vec3(0, 0, -1), 0.5))
-  world.add(new Sphere(new Vec3(0, -100.5, -1), 100))
+  world.add(new Sphere([0, 0, -1], 0.5))
+  world.add(new Sphere([0, - 100.5, - 1], 100))
 
   for (let j = IMAGE_HEIGHT - 1; j >= 0; j--) {
-    // console.log(j)
     for (let i = 0; i < IMAGE_WIDTH; i++) {
-      let pixelColor = new Color(0, 0, 0)
+      let pixelColor = [0, 0, 0] // new Color(0, 0, 0)
       for (let s = 0; s < SAMPLE_PER_PIXEL; s++) {
         let u = (i + Math.random()) / (IMAGE_WIDTH - 1)
         let v = (j + Math.random()) / (IMAGE_HEIGHT - 1)
         let r = camera.getRay(u, v)
-        pixelColor.addThis(r.rayColor(world))
+        pixelColor = add(pixelColor, rayColor(r, world))
       }
-      pixelColor.draw(ctx, DRAWING_OFFSET_X + i, (DRAWING_OFFSET_Y  + IMAGE_HEIGHT) - j)
+      drawPixel(pixelColor, ctx, DRAWING_OFFSET_X + i, (DRAWING_OFFSET_Y  + IMAGE_HEIGHT) - j)
     }
   }
 }
-
-
 
 const loop = () => {
   requestAnimationFrame(loop)
---
+++ javascriptVersion/indexSave.js
@@ -0,0 +1,256 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+ctx.canvas.width = window.innerWidth
+ctx.canvas.height = window.innerHeight
+ctx.font = '15px serif'
+
+
+const DRAWING_OFFSET_X = 50
+const DRAWING_OFFSET_Y = 50
+
+const SAMPLE_PER_PIXEL = 50
+const MAX_DEPTH = 50
+
+const IMAGE_WIDTH = 100
+const ASPECT_RATIO = 16.0 / 9.0
+const IMAGE_HEIGHT = Math.floor(IMAGE_WIDTH / ASPECT_RATIO)
+
+
+class Vec3 {
+  constructor(x, y, z) {
+    this.x = x
+    this.y = y
+    this.z = z
+  }
+
+  add = (vec) => new Vec3(this.x + vec.x, this.y + vec.y, this.z + vec.z)
+  sub = (vec) => new Vec3(this.x - vec.x, this.y - vec.y, this.z - vec.z)
+  mul = (val) => new Vec3(this.x * val, this.y * val, this.z * val)
+  div = (val) => new Vec3(this.x / val, this.y / val, this.z / val)
+
+  dot = (vec) => this.x * vec.x + this.y * vec.y + this.z * vec.z
+  lengthSquared = () => this.x * this.x + this.y * this.y + this.z * this.z
+  length = () => Math.sqrt(this.lengthSquared())
+  unitVector = () => this.div(this.length())
+}
+
+let a = new Vec3(0, 0, 0)
+let b = a.add(new Vec3(2, 1, 0.5)).add(new Vec3(2, 1, 0.5)).div(4)
+console.log(b)
+
+
+class Camera {
+  constructor() {
+    this.aspectRatio = 16.0 / 9.0
+    this.viewportHeight = 2.0
+    this.viewportWidth = this.aspectRatio * this.viewportHeight
+    this.focalLength = 1.0
+
+    this.origin = new Vec3(0, 0, 0)
+    this.horizontal = new Vec3(this.viewportWidth, 0, 0)
+    this.vertical = new Vec3(0.0, this.viewportHeight, 0.0)
+    this.lowerLeftCorner = this.origin.sub(this.horizontal.div(2)).sub(this.vertical.div(2)).sub(new Vec3(0, 0, this.focalLength))
+  }
+
+  getRay = (u, v) => new Ray(this.origin, this.lowerLeftCorner.add(this.horizontal.mul(u)).add(this.vertical.mul(v)).sub(this.origin))
+}
+
+class Color {
+  constructor(r, g, b) {
+    this.r = r
+    this.g = g
+    this.b = b
+  }
+
+  draw = (ctx, x, y) => {
+    this.divThis(SAMPLE_PER_PIXEL)
+    ctx.fillStyle = `rgb(${this.clamp(this.r, 0.0, 0.999) * 256}, ${this.clamp(this.g, 0.0, 0.999) * 256}, ${this.clamp(this.b, 0.0, 0.999) * 256})`
+    ctx.fillRect(x, y, 1, 1)
+  }
+
+  // clamp = (x, min, max) => x < min ? min : x > max ? : max : x
+  clamp = (x, min, max) => {
+    if (x < min) return min
+    if (x > max) return max
+    return x
+  }
+
+  add = (col) => new Color(this.r + col.r, this.g + col.g, this.b + col.b)
+  mul = (val) => new Color(this.r * val, this.g * val, this.b * val)
+  div = (val) => new Color(this.r / val, this.g / val, this.b / val)
+
+  addThis = (col) => {
+    this.r += col.r
+    this.g += col.g
+    this.b += col.b
+  }
+
+  divThis = (val) => {
+    this.r /= val
+    this.g /= val
+    this.b /= val
+  }
+
+}
+
+const randomInUnitSphere = () => {
+  while (true) {
+    let p = new Vec3(- 1 + 2 * Math.random(), - 1 + 2 * Math.random(), - 1 + 2 * Math.random())
+    if (p.lengthSquared() < 1) return p
+  }
+}
+
+class Ray {
+  constructor(origin, direction) {
+    this.origin = origin
+    this.direction = direction
+  }
+
+  at = t => this.origin.add(this.direction.mul(t))
+
+  rayColor = (world, depth) => {
+    let rec = new HitRecord()
+    if (depth <= 0) return new Color(0, 0, 0)
+    let [hit, returnedRecord] = world.hit(this, 0, Infinity, rec)
+    if (hit) {
+      let target = rec.p.add(rec.normal).add(randomInUnitSphere())
+      // let color = new Color(returnedRecord.normal.x, returnedRecord.normal.y, returnedRecord.normal.z)
+      // return color.add(new Color(1, 1, 1)).mul(0.5)
+      this.origin = returnedRecord.p
+      this.direction = target.sub(returnedRecord.p)
+      return this.rayColor(world, depth - 1).mul(0.5)
+    }
+    let unitDirection = this.direction.unitVector()
+    let t = 0.5 * unitDirection.y + 1.0
+    return new Color(1, 1, 1).mul(1.0 - t).add(new Color(0.5, 0.7, 1.0).mul(t))
+  }
+}
+
+const hitSphere = (center, radius, r) => {
+  let oc = r.origin.sub(center)
+  let a = r.direction.lengthSquared()
+  let halfB = oc.dot(r.direction)
+  let c = oc.lengthSquared() - radius * radius
+  let discriminant = halfB * halfB - a * c
+  return discriminant < 0 ? - 1.0 : (- halfB - Math.sqrt(discriminant)) / a
+}
+
+class HitRecord {
+  constructor() {
+    this.p = new Vec3(0, 0, 0)
+    this.normal = new Vec3(0, 0, 0)
+    this.t = 0
+    this.frontFace = false
+  }
+
+  setFaceNormal = (r, outwardNormal) => {
+    // if (r.direction.dot(outwardNormal) < 0) {
+    //   this.frontFace = true
+    // } else {
+    //   this.frontFace = false
+    // }
+    // if (this.frontFace === true) {
+    //   this.normal =
+    // }
+    this.frontFace = r.direction.dot(outwardNormal) < 0
+    this.normal = this.frontFace ? outwardNormal : outwardNormal.mul(-1)
+  }
+}
+
+class Sphere {
+  constructor(center, radius) {
+    this.center = center
+    this.radius = radius
+  }
+
+  hit = (r, tMin, tMax, rec) => {
+    let oc = r.origin.sub(this.center)
+    let a = r.direction.lengthSquared()
+    let halfB = oc.dot(r.direction)
+    let c = oc.lengthSquared() - this.radius * this.radius
+    let discriminant = halfB * halfB - a * c
+
+    if (discriminant > 0) {
+      let root = Math.sqrt(discriminant)
+      let temp = (- halfB - root) / a
+      if (temp < tMax && temp > tMin) {
+        rec.t = temp
+        rec.p = r.at(rec.t)
+        let outwardNormal = (rec.p.sub(this.center).div(this.radius))
+        rec.setFaceNormal(r, outwardNormal)
+        return [true, rec]
+      }
+      temp = (-halfB + root) / a
+      if (temp < tMax && temp > tMin) {
+        rec.t = temp
+        rec.p = r.at(rec.t)
+        let outwardNormal = (rec.p.sub(this.center).div(this.radius))
+        rec.setFaceNormal(r, outwardNormal)
+        return [true, rec]
+      }
+    }
+    return [false, rec]
+  }
+}
+
+class HittableList {
+  constructor() {
+    this.objects = []
+  }
+
+  clear = () => this.objects = []
+  add = object => this.objects.push(object)
+
+  hit = (r, tMin, tMax, rec) => {
+    // rayon, record vide au debut
+    let tempRec = new HitRecord()
+    let hitAnything = false
+    let closestSoFar = tMax
+    this.objects.forEach(object => {
+      let [hitten, returnedRecord] = object.hit(r, tMin, closestSoFar, tempRec)
+      if (hitten) {
+        hitAnything = true
+        closestSoFar = returnedRecord.t
+        rec = returnedRecord
+      }
+      // if (object.hit(r, tMin, closestSoFar, tempRec)) {
+      //   hitAnything = true
+      //   closestSoFar = tempRec.t
+      //   rec = tempRec
+      // }
+    })
+
+    return [hitAnything, rec]
+  }
+}
+
+const render = (ctx, canvas) => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  let camera = new Camera()
+  let world = new HittableList()
+  world.add(new Sphere(new Vec3(0, 0, -1), 0.5))
+  world.add(new Sphere(new Vec3(0, -100.5, -1), 100))
+
+  for (let j = IMAGE_HEIGHT - 1; j >= 0; j--) {
+    // console.log(j)
+    for (let i = 0; i < IMAGE_WIDTH; i++) {
+      let pixelColor = new Color(0, 0, 0)
+      for (let s = 0; s < SAMPLE_PER_PIXEL; s++) {
+        let u = (i + Math.random()) / (IMAGE_WIDTH - 1)
+        let v = (j + Math.random()) / (IMAGE_HEIGHT - 1)
+        let r = camera.getRay(u, v)
+        pixelColor.addThis(r.rayColor(world, MAX_DEPTH))
+      }
+      pixelColor.draw(ctx, DRAWING_OFFSET_X + i, (DRAWING_OFFSET_Y  + IMAGE_HEIGHT) - j)
+    }
+  }
+}
+
+
+
+const loop = () => {
+  requestAnimationFrame(loop)
+}
+
+render(ctx, canvas)
+loop()
